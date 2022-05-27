e0724f02ea1cb0081a93c5dfb3930a4f300ed8d2
Hemalatha <hemalatha@divami.com>
2022-05-27 21:05:32 +0530

    Add gitignore and update database details

--- db_config.js
+++ db_config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD:"",
+    HOST: "demo.divami.com",
+    USER: "hemalatha",
+    PASSWORD:"hemalathahackathon",
+    PORT: 3306,
     DB: "training_management",
     dialect: "mysql",
     pool: {
